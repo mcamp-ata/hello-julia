dcb825f2c833a322c5b9f6bedfad0b8da6869767
Matt Camp <matt@aicampground.com>
2023-10-18 13:41:37 -0500

    wip

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.poetry2nix = {
-    url = "github:nix-community/poetry2nix";
+    url = "github:nix-community/poetry2nix/master";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
