53097621c20e85d0c703e432e0902c7d3bc3358a
Matt Camp <matt@aicampground.com>
2023-10-25 15:11:05 -0500

    more refined

--- flake.nix
+++ flake.nix
@@ -4,72 +4,125 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  inputs.devshell.url = "github:numtide/devshell";
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }: flake-utils.lib.eachDefaultSystem (system:
-    let 
-      pkgs = import nixpkgs { inherit system; };
-      startPlutoScript = pkgs.writeText "start_pluto.jl" ''
-        using Pkg
-        Pkg.add("Pluto")
-        Pkg.add("ArgParse")
-        using Pluto
-        using ArgParse
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
 
-        function main()
-            s = ArgParseSettings()
-
-            @add_arg_table s begin
-                "--port", "-p"
-                    arg_type = Int
-                    default = 1234
-                    help = "Port number to start Pluto on."
-            end
-
-            parsed_args = parse_args(ARGS, s)
-
-            port = parsed_args["port"]
-
-            Pluto.run(port=port)
-        end
-
-        main()
-      '';
-    in
-    {
-      devShell = pkgs.mkShell {
-        buildInputs = [
-          pkgs.python3
-          pkgs.poetry
-          pkgs.zlib
-          pkgs.libstdcxx5
-          pkgs.gcc
-          pkgs.julia
-
-        ];
-        shellHook = ''
-          export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
-          export LD_LIBRARY_PATH="${pkgs.gcc.cc.lib}/lib:${pkgs.zlib}/lib:$LD_LIBRARY_PATH"
-          export PYTHON="${pkgs.python3}/bin/python"
-          export JULIA_CONDAPKG_BACKEND="Null"
-          export JULIA_PYTHONCALL_EXE="${pkgs.python3}/bin/python"
-          zsh
-        '';
-      };
-
-      packages = {
-        hello-world = poetry2nix.mkPoetryApplication {
+  outputs = { self, nixpkgs, devshell, flake-utils, poetry2nix, ... }: 
+    flake-utils.lib.eachDefaultSystem (system: {
+    apps.devshell = self.outputs.devShells.${system}.default.flakeApp;
+    devShells.default = 
+      let 
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [
+            devshell.overlays.default
+          ];
+        };
+        # This is required if you get odd errors
+        # read the https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md
+        pypkgs-build-requirements = {
+          pyjulia = [ "setuptools"];
+          julia = [ "setuptools" ];
+          juliapkg = [ "setuptools" ];
+          urllib3 = [ "hatchling" ];
+          juliacall = [ "setuptools" ];
+          pandas = [ "versioneer" ];
+          # contourpy = [ "mesonpy" ];
+          # numpy = [ "setuptools" ];
+        };
+        p2n-overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super:
+          builtins.mapAttrs (package: build-requirements:
+            (builtins.getAttr package super).overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or [ ]) ++ (builtins.map (pkg: if builtins.isString pkg then builtins.getAttr pkg super else pkg) build-requirements);
+            })
+          ) pypkgs-build-requirements
+        );
+        hello-world = pkgs.poetry2nix.mkPoetryEnv  {
           projectDir = ./.;
           python = pkgs.python3;
+          overrides = p2n-overrides;
+          preferWheels = true;
         };
-        run-hello-world = pkgs.writeShellScriptBin "run-hello-world" ''
-          ${pkgs.poetry}/bin/poetry run python ${./hello.py}
-        '';
-        pluto = pkgs.writeShellScriptBin "pluto" ''
-          ${pkgs.julia}/bin/julia ${startPlutoScript}
-        '';
-      };
+        thisProject = pkgs.stdenv.mkDerivation {
+          name = "boat_models";
+          src = ./.;  # Copy the entire project directory into the Nix store
+          installPhase = ''
+            mkdir -p $out
+            cp -r ./* $out/
+          '';
+        };
+        hello_py = pkgs.writeText "hello.py" (builtins.readFile ./hello.py);
 
-      defaultPackage = self.packages.${system}.hello-world;
-    }
-  );
+        startPlutoScript = pkgs.writeText "start_pluto.jl" ''
+          using Pkg
+          Pkg.add("Pluto")
+          Pkg.add("ArgParse")
+          using Pluto
+          using ArgParse
+
+          function main()
+              s = ArgParseSettings()
+
+              @add_arg_table s begin
+                  "--port", "-p"
+                      arg_type = Int
+                      default = 1234
+                      help = "Port number to start Pluto on."
+              end
+
+              parsed_args = parse_args(ARGS, s)
+
+              port = parsed_args["port"]
+
+              Pluto.run(port=port)
+          end
+
+          main()
+        '';
+        pluto = pkgs.writeShellScript "pluto" ''
+        ${pkgs.julia}/bin/julia ${startPlutoScript}
+        '';
+        randomFiglet = pkgs.writeShellScript "randomFiglet" ''
+          phrases=("Nix Your Problems Away!" "One Flake to Rule Them All!" "Why Fix It, When You Can Nix It?" "Nixify Life" "Just Nix It" "Powered by Nix")
+          random_index=$((RANDOM % 6))
+          case $random_index in
+            0) ${pkgs.figlet}/bin/figlet "Nixify Life";;
+            1) ${pkgs.figlet}/bin/figlet "Just Nix It";;
+            2) ${pkgs.figlet}/bin/figlet "Powered by Nix";;
+            3) ${pkgs.figlet}/bin/figlet "Nix Your Problems Away!";;
+            4) ${pkgs.figlet}/bin/figlet "One Flake to Rule Them All!";;
+            5) ${pkgs.figlet}/bin/figlet "Why Fix It, When You Can Nix It?";;
+          esac
+        '';
+      in
+      pkgs.devshell.mkShell ({config,...}: {
+        imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+        name = "boat-model";
+        motd = ''
+                 {214}👁️  Welcome to Boat Model devshell 👁️{reset}
+                 $( ${randomFiglet} | ${pkgs.lolcat}/bin/lolcat -f )
+                 $(type -p menu &>/dev/null && menu)
+               '';
+        commands = [
+          {
+            name = "pluto";
+            command = ''
+            ${pkgs.julia}/bin/julia ${startPlutoScript}
+            '';
+          }
+          {
+            # TODO: Fix poetry environment
+            name = "hello-world";
+            command = ''
+            ${hello-world}/bin/python3 ${thisProject}/hello.py
+            '';
+          }
+        ];
+      });
+  });
 }
--- hello.py
+++ hello.py
@@ -1,4 +1,13 @@
 from juliacall import Main as jl
-jl.include("./hello.jl")
-result = jl.hello("Python! I am coming to you live from Julia!!")
-print(result)
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+julia_file_path = os.path.join(current_dir, "hello.jl")
+
+jl.include(julia_file_path)
+
+def main():
+    result = jl.hello("Python! I am coming to you live from Julia!!")
+    print(result)
+
+if __name__ == "__main__":
+    main()
